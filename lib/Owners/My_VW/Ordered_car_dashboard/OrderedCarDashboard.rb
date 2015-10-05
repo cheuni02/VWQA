0ce3dad6046da6f97221b166cc53d936e32ae065
Gowtham <gowthamselvarajah@gmail.com>
2015-10-05 16:15:28 +0100

    removed code from lib file

--- lib/Owners/My_VW/Ordered_car_dashboard/OrderedCarDashboard.rb
+++ lib/Owners/My_VW/Ordered_car_dashboard/OrderedCarDashboard.rb
@@ -3,30 +3,7 @@ class OrderedCarDashboard < MyVW
   def visit
     visit_page(page_url)
   end
-=begin
-  def login_account_ordered
-    get_credentials_ordered
-    set_username.set("#{username_ordered}")
-    set_password.set("#{password_ordered}")
-    click_login_button
-  end
 
-  def get_credentials_ordered
-    grab_user_cred_ordered
-  end
-
-  def set_username
-    username_field
-  end
-
-  def set_password
-    password_box
-  end
-
-  def click_login_button
-    logon_button.click
-  end
-=end
   def ordered_car_hero_present
     ordered_car_hero.present?
   end
@@ -73,34 +50,7 @@ class OrderedCarDashboard < MyVW
   def page_url
     "/vw-authentication/login/auth?targetUrl=/owners/my/cars"
   end
-=begin
-  def username_ordered
-    @username_user_ordered
-  end
 
-  def password_ordered
-    @password_user_ordered
-  end
-
-  def username_field
-    @browser.text_field(:id => "username")
-  end
-
-  def password_box
-    @browser.text_field(:id => "password")
-  end
-
-  def grab_user_cred_ordered
-    user_data = File.read('users.json')
-    users_hash = JSON.parse(user_data)
-    @username_user_ordered = users_hash["User_accounts"]["Users_ordered_car"][0]["Username"]
-    @password_user_ordered = users_hash["User_accounts"]["Users_ordered_car"][0]["Password"]
-  end
-
-  def logon_button
-    @browser.button(:id => 'login-button')
-  end
-=end
   def ordered_car_hero
     @browser.section(:class => "ordered-car-hero--step-04")
   end
