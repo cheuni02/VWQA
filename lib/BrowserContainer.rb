d24c2e06097b608e28ded9895cf0b5449b544b15
Dragonz VM <craig.merrilees@tribalworldwide.co.uk>
2015-10-05 12:24:30 +0100

    Remove MPV Content from VW-FrontEnd Repo
    This content has now moved to its own frontend repository at
    https://stash.tribalddb.co.uk/projects/VWQA/repos/vw-mpv/browse

--- lib/BrowserContainer.rb
+++ lib/BrowserContainer.rb
@@ -15,39 +15,6 @@ class BrowserContainer
   end
 
   def visit_page(page_url = "")
-=begin
-    case ENV['TAG'].to_sym
-    when :dev01, :stage_v
-      url = URLS.gsub('http://', "http://#{VW_USER}:#{VW_PASSWORD}@")  + page_url
-      #STDOUT.puts url
-      @browser.goto(url.sub('http', 'https'))
-      #@browser.goto(url)
-      #url = URLS.gsub('http://', "http://#{VW_USER}:#{VW_PASSWORD}@")  + page_url
-    when :int, :uat
-      url = URLS.gsub("http://", "https://#{VW_USER}:#{VW_PASSWORD}@") + page_url
-      #STDOUT.puts url
-      @browser.goto(url)
-      @browser.goto(url.sub('https', 'http'))
-    else
-      @browser.goto(URLS + page_url)
-    end
-=end
     @browser.goto(URLS + page_url)
   end
-
-  def visit_page_mpv(page_url = "")
-    case ENV['TAG'].to_sym
-    when :stage
-     base_url = ("https://#{VW_USER}:#{VW_PASSWORD}@perfect-stage-vw.tribalddb.co.uk")
-    when :live
-      base_url = ("https://perfect.volkswagen.co.uk")
-    end
-    #base_url = ("https://perfect.volkswagen.co.uk")
-    #STDOUT.puts base_url + page_url
-    @browser.goto (base_url + page_url) if (ENV['TAG'].to_sym == :stage || ENV['TAG'].to_sym == :live)
-    @browser.refresh
-  end
-
-
-
 end
