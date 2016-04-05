7e9f31d2daa214650c6ee94a95878803942a48d3
Chinese red dragon <ivan.cheung@tribalddb.co.uk>
2016-04-05 15:24:55 +0100

    added a script to remove the tbl_env table

---
+++ util/migration_scripts/rollback_remove_tables.rb
@@ -0,0 +1,19 @@
+require 'json'
+require 'dbi'
+require 'open-uri'
+
+DBI.connect("DBI:Mysql:user_db_ivan_machine:localhost","root","Tribaluser12") do |dbh|
+
+  #remove env_tbl
+  begin
+    sth = dbh.prepare("DROP TABLE IF EXISTS tbl_env")
+    sth.execute
+
+  rescue DBI::DatabaseError => e
+    puts "ErrorCode : #{e.err}"
+
+  end
+
+  sth.finish
+
+end
--- util/migration_scripts/user_json_to_table.rb
+++ util/migration_scripts/user_json_to_table.rb
@@ -2,36 +2,60 @@ require 'json'
 require 'dbi'
 require 'open-uri'
 
-begin
 
-file = File.read("../../users.json")
-json = JSON.parse(file)
 
-#check if 'env' and 'user_acc' tables exist. If not create them
-  dbh = DBI.connect("DBI:Mysql:user_db_ivan_machine:localhost","root","Tribaluser12")
 
-  sth = dbh.prepare("SELECT * FROM tbl_env")
-  sth.execute
+users_file = JSON.parse(File.read("../../users.json"))
 
-  sth.fetch do |row|
+envs = users_file['User_accounts']
 
-    puts "1) so far tbl_env table rows: "
-    puts "id is #{row[0]}"
-    puts "url is #{row[1]}"
+
+DBI.connect("DBI:Mysql:user_db_ivan_machine:localhost","root","Tribaluser12") do |dbh|
+
+
+  ## check table exists
+  begin
+    sth = dbh.prepare("SELECT * FROM tbl_env")
+    sth.execute
+  rescue DBI::DatabaseError => e
+
+    puts "error code: #{e.err}..."
+    case e.err
+      when 1146
+        puts "tbl_env not created! creating it ..."
+        sth = dbh.prepare("CREATE TABLE tbl_env (env_id INT NOT NULL AUTO_INCREMENT, env_url VARCHAR(40), PRIMARY KEY(env_id));")
+        sth.execute
+    end
+    retry
 
   end
 
-  puts "inserting data into table ..."
 
-  sth = dbh.prepare("INSERT INTO tbl_env (env_url) VALUES ('www.volkswagen.co.uk'), ('origin.volkswagen.co.uk');")
-  sth.execute
+  ## if exists, input rows from JSON file to table
+  # puts "inserting data into table ..."
+  #
+  # sth = dbh.prepare("INSERT INTO tbl_env (env_url) VALUES ('www.volkswagen.co.uk'), ('origin.volkswagen.co.uk');")
+  # sth.execute
 
+  puts "list of environments to be inserted to tb_envs table:"
+
+
+
+  envs.each do |a|
+
+    puts a[0]#["username"]
+     sth = dbh.prepare("INSERT INTO tbl_env (env_url) VALUES (?);")
+     sth.execute(a[0])
+  end
+
+
+  ## display table
   sth = dbh.prepare("SELECT * FROM tbl_env")
   sth.execute
 
   sth.fetch do |row|
 
-    puts "2) now tbl_env table rows: "
+    puts "now tbl_env table rows: "
     puts "id is #{row[0]}"
     puts "url is #{row[1]}"
 
@@ -40,15 +64,5 @@ json = JSON.parse(file)
 
   sth.finish
 
-rescue DBI::DatabaseError => e
-
- puts "error code: #{e.err}..."
- case e.err
-   when 1146
-     puts "tbl_env not created! creating it ..."
-     sth = dbh.prepare("CREATE TABLE tbl_env (env_id INT NOT NULL AUTO_INCREMENT, env_url VARCHAR(40), PRIMARY KEY(env_id));")
-     sth.execute
- end
- retry
 
 end
