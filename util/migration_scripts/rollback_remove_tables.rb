5da8d0efe83c804d41ffcb467dfe50b57884922f
Chinese red dragon <ivan.cheung@tribalddb.co.uk>
2016-04-06 17:36:38 +0100

    grouped functions create and display table

--- util/migration_scripts/rollback_remove_tables.rb
+++ util/migration_scripts/rollback_remove_tables.rb
@@ -4,13 +4,20 @@ require 'open-uri'
 
 DBI.connect("DBI:Mysql:user_db_ivan_machine:localhost","root","Tribaluser12") do |dbh|
 
+  #str = "select * from \
+  #      tbl_env"
   #remove env_tbl
+
+
+
   begin
-    sth = dbh.prepare("DROP TABLE IF EXISTS tbl_env")
+    sth = dbh.prepare('DROP TABLE IF EXISTS tbl_usr;')
     sth.execute
 
-    sth = dbh.prepare("DROP TABLE IF EXISTS tbl_usr")
+    sth = dbh.prepare('DROP TABLE IF EXISTS tbl_env;')
     sth.execute
+
+
 
   rescue DBI::DatabaseError => e
     puts "ErrorCode : #{e.err}"
--- util/migration_scripts/user_json_to_table.rb
+++ util/migration_scripts/user_json_to_table.rb
@@ -37,12 +37,34 @@ class TableHandler
     end
   end
 
-  def insert_data(tbl_name)
+  def insert_env_data(tbl_name)
     envs.each do |a|
       puts a[0]
       sth = db_connect.prepare("INSERT INTO #{tbl_name} (env_url) VALUES (?);")
       sth.execute(a[0])
     end
+  end
+
+  def insert_data
+    i=0
+    envs.each do |a|
+      puts "env = #{a[0]}"
+      # sth = db_connect.prepare("INSERT INTO tbl_env (env_url) VALUES (?);")
+      # sth.execute(a[0])
+
+
+
+      a[0].each do |b|
+        puts "other attributes = #{b}"
+      end
+
+      i+=1
+      break if i == 1
+    end
+
+    # sth = db_connect.prepare("INSERT INTO tbl_env (env_url) VALUES (?);")
+    # puts "envs['User_accounts'] = #{envs}"
+
   end
 
   def display_table(tbl_name)
@@ -61,27 +83,44 @@ class TableHandler
 end
 
 
-  ## check env table exists and if not create it
+tbl_env_headers = "env_id INT NOT NULL AUTO_INCREMENT, env_url VARCHAR(40), PRIMARY KEY(env_id)"
+tbl_usr_headers = "usr_id INT NOT NULL AUTO_INCREMENT, env_id INT NOT NULL, usr_username VARCHAR(40), \
+                  usr_title VARCHAR(40), usr_lastname VARCHAR(40), usr_password VARCHAR(40), \
+                  usr_purpose VARCHAR(40), uuid INT, optional_details_id INT, PRIMARY KEY(usr_id), \
+                  FOREIGN KEY(env_id) REFERENCES tbl_env(env_id)"
+tbl_option_headers = "option_id INT NOT NULL AUTO_INCREMENT, usr_id INT, address_type VARCHAR(20), \
+                  house_number int(4), postcode VARCHAR(10), street VARCHAR(40), street2 VARCHAR(40), \
+                  city VARCHAR(30), county VARCHAR(30), phone1 INT(20), phone2 INT(20), phone_type VARCHAR(10), \
+                  work_phone INT(20), preferred_contact VARCHAR(10), marital_status VARCHAR(20), \
+                  date_of_birth VARCHAR(30), PRIMARY KEY (option_id), FOREIGN KEY(usr_id) REFERENCES tbl_usr(usr_id)"
+
+
+  ############ check env, usr and options tables exist and if not create them
 
   hand = TableHandler.new
 
+  ## 1) check env table exists and if not create it
+  hand.create_table('tbl_env',tbl_env_headers)
 
-  tbl_headers = "env_id INT NOT NULL AUTO_INCREMENT, env_url VARCHAR(40), PRIMARY KEY(env_id)"
-  hand.create_table('tbl_env',tbl_headers)
+  ## 2) check user table exists and if not create it
+  hand.create_table('tbl_usr',tbl_usr_headers)
 
-  ## insert data
+  ## 3) check optional_details table exists and if not create it
+  hand.create_table('tbl_usr',tbl_option_headers)
+
+
+  ############ insert data
+
   puts "list of environments to be inserted to tb_envs table:"
-  hand.insert_data('tbl_env')
 
-  ## display table
-  hand.display_table('tbl_env')
-
-  ## check user table exists and if not create it
-
-  tbl_headers = "usr_id INT NOT NULL AUTO_INCREMENT, env_id INT NOT NULL, usr_username VARCHAR(40), usr_title VARCHAR(40), usr_lastname VARCHAR(40), usr_password VARCHAR(40), usr_purpose VARCHAR(40), uuid INT, optional_details_id INT, PRIMARY KEY(usr_id), FOREIGN KEY(env_id) REFERENCES tbl_env(env_id)"
-  hand.create_table('tbl_usr',tbl_headers)
+  ## 1) insert tbl_env from user.json in sequential order
+  hand.insert_data
 
 
+
+
+  ############ display tables
+  #hand.display_table('tbl_env')
 
 
 
